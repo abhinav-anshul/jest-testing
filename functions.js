2d6d8889d878e950651ab29b3722f8b81156c5c1
abhinav anshul <abhinavanshul4u@gmail.com>
2020-03-08 18:22:14 +0530

    added isNUll matcher

--- functions.js
+++ functions.js
@@ -2,7 +2,8 @@ const functions = {
   add1: (num1, num2) => num1 + num2,
   add2: function(num3, num4) {
     return num3 + num4;
-  }
+  },
+  isNull: () => null
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,11 @@ test("Adds 2 + 2 to equal 4", () => {
 test("Adds 3 + 3", () => {
   expect(functions.add2(3, 3)).toBe(6);
 });
+
+test("Adds 3 + 3", () => {
+  expect(functions.add2(3, 3)).not.toBe(10);
+});
+
+test("should be NULL", () => {
+  expect(functions.isNull()).toBeNull();
+});
