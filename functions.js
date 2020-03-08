cf234e5f77255c82ca7ae844e482a8e8c2981404
abhinav anshul <abhinavanshul4u@gmail.com>
2020-03-08 18:08:59 +0530

    more function added

--- functions.js
+++ functions.js
@@ -1,5 +1,8 @@
 const functions = {
-  add1: (num1, num2) => num1 + num2
+  add1: (num1, num2) => num1 + num2,
+  add2: function(num3, num4) {
+    return num3 + num4;
+  }
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require("./functions");
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add1(2, 2)).toBe(4);
 });
+
+test("Adds 3 + 3", () => {
+  expect(functions.add2(3, 3)).toBe(6);
+});
